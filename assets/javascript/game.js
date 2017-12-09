65cf504f23d41635e59ac6084119bf7dd267ce78
Christina Bertram <Jamerican92grl@ChristinasMBP2.home>
2017-12-09 00:35:21 -0500

    Placed images and titles

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+// <!-- Game sarts when a (any) button is pressed -->
+
+// <!-- A random word from a group of words is secretly selected -->
+
+const wordSelection = ["Shipoopi", "Cool Whip", "Evil Monkey", "Surfin' Bird", "Road House"]
+
+// <!-- User presses a letter key to guess a letter
+//  -->
+
+
+
+// <!-- If they are wrong, that letter is displayed (at the bottom) and taken out of the options. The count for the number of guesses decreases. -->
+
+// <!-- If they run out of guesses, the a "you lose" song plays and a display pops up -->
+
+// <!-- If they are right the letter is displayed (at the top) and taken out of the options -->
+
+// <!-- If they guesses all the right letters, A photo/video pops us and a song plays. -->
+
+// <!-- The game restarts -->
